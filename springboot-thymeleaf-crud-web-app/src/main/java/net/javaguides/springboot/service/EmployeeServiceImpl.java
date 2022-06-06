f71ea4a442dfbe70af1ddb8d12cba6d195c5a510
KhaiHoanNB <105690960+KhaiHoanNB@users.noreply.github.com>
2022-06-06 09:37:46 +0700

    EmploymentManagement

---
+++ springboot-thymeleaf-crud-web-app/src/main/java/net/javaguides/springboot/service/EmployeeServiceImpl.java
@@ -0,0 +1,40 @@
+package net.javaguides.springboot.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Override
+	public List<Employee> getAllEmployees() {
+		
+		return employeeRepository.findAll();
+	}
+
+	@Override
+	public void saveEmployee(Employee employee) {
+		this.employeeRepository.save(employee);
+		
+	}
+
+	@Override
+	public Employee getEmployeeById(long id) {
+		
+		return employeeRepository.getById(id);
+	}
+
+	@Override
+	public void deleteEmployeeById(long id) {
+		this.employeeRepository.deleteById(id);
+		
+	}
+
+}
